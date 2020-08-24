1a03a1cd6ec336ff2b116b8df43a411e78bc8e99
GAMELEIRA <gameleira270499@gmail.com>
2020-08-24 17:43:12 -0300

    Adicionado o modulo organizacao.js na pasta FUNDAMENTOS que têm como objetivo apresentar as sentenças de código JavaScript

---
+++ FUNDAMENTOS/organizacao.js
@@ -0,0 +1,21 @@
+/**
+ * AUTOR: GABRIEL GAMELEIRA DOS SANTOS
+ * DATA: 24/08/2020
+ * HORA: 17:32
+ * FINALIDADE DE PROGRAMA: APRESENTAR A ESTRUTURA DE UM CODIGO JAVASCRIPT
+ */
+
+ //SETENCA DE CODIGO QUEBRADA EM DUAS LINHAS
+console.log("Setença "+
+                        +" de código!");
+
+//BLOCO DE CODIGO 1
+{
+    //BLOCO INTERNO AO BLOCO DE CODIGO 1
+    {
+        console.log("Olá!");
+        
+        console.log('Mundo!');
+        
+    }
+}
