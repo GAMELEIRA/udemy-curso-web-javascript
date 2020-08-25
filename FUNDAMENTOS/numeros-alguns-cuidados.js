646ff9d1b282f89bbdf4fb20c34bcee8a0d1cc1b
GAMELEIRA <gameleira270499@gmail.com>
2020-08-24 23:52:51 -0300

    Adicionado o módulo numeros-alguns-cuidados.js na pasta FUNDAMENTOS que têm como objetivo mostrar algumas inconsistencias que o tipo  numerico pode oferecer.

---
+++ FUNDAMENTOS/numeros-alguns-cuidados.js
@@ -0,0 +1,20 @@
+/**
+ * AUTOR: GABRIEL GAMELEIRA DOS SANTOS
+ * DATA: 24/08/2020
+ * HORA: 23:50
+ * FINALIDADE DE PROGRAMA: APRESENTAR INCONSITENCIAS NO TIPO Number
+ */ 
+
+console.log(7 / 0); //RETORNA O TIPO Infity
+
+console.log("10" / 2); //REALIZA A OPERACAO NORMALMENTE
+
+console.log("Show!" * 2); //RETORNA NAN
+
+console.log(0.1 + 0.7); //IMPRECISAO
+
+// console.log(90.toFixed(2));
+
+console.log((90).toFixed(2));
+
+console.log((90).toFixed(1).toString());
