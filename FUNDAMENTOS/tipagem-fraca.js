a8c618cf72bf31333f01ffea115b6b95e7d4152d
GAMELEIRA <gameleira270499@gmail.com>
2020-08-24 23:16:22 -0300

    Adicionado o módulo tipagem-fraca.js na pasta FUNDAMENTOS que têm como objetivo introduzir os tipos  em js.

---
+++ FUNDAMENTOS/tipagem-fraca.js
@@ -0,0 +1,18 @@
+/**
+ * AUTOR: GABRIEL GAMELEIRA DOS SANTOS
+ * DATA: 24/08/2020
+ * HORA: 23:12
+ * FINALIDADE DE PROGRAMA: APRESENTAR A OS TIPOS EM JS 
+ */
+
+let qualquer = 'Gabriel';
+
+console.log(qualquer);
+
+console.log(typeof qualquer); //typeof retorna o tipo do valor
+
+qualquer = 3.1416;
+
+console.log(qualquer);
+
+console.log(typeof qualquer); //typeof retorna o tipo do valor
