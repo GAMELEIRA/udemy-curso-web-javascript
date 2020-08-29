fee8bdeeacda556cc3ef2cfe6a99183135a91167
GAMELEIRA <gameleira270499@gmail.com>
2020-08-29 02:36:50 -0300

    Adicionado o modulo objeto2.js na pasta FUNDAMENTOS que têm como objetivo apresentar os conceitos relacionados aos tipos OBJECT e FUNCTION

---
+++ FUNDAMENTOS/objeto2.js
@@ -0,0 +1,22 @@
+/**
+ * AUTOR: GABRIEL GAMELEIRA DOS SANTOS
+ * DATA: 29/08/2020
+ * HORA: 02:36
+ * FINALIDADE DE PROGRAMA: APRESENTAR CONCEITOS RELACIONADOS A OBJECT E FUNCTIONS
+ */
+
+console.log(typeof Object); //FUNCTION
+
+console.log(typeof new Object); //OBJ
+
+const Cliente = function () {} 
+
+console.log(typeof Cliente);//FUNCTION
+
+console.log(typeof new Cliente);//OBJ
+
+class Produto {}
+
+console.log(typeof Produto);//FUNCTION
+
+console.log(typeof new Produto); //OBJ
