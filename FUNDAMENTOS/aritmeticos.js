7f6ff4f6bb1301ccf741c4a9d27272bca67256d6
GAMELEIRA <gameleira270499@gmail.com>
2020-08-29 05:12:29 -0300

    Adicionado o modulo aritmeticos.js na pasta FUNDAMENTOS que têm como objetivo trabalhar com operadores aritmeticos

---
+++ FUNDAMENTOS/aritmeticos.js
@@ -0,0 +1,21 @@
+/**
+ * AUTOR: GABRIEL GAMELEIRA DOS SANTOS
+ * DATA: 25/08/2020
+ * HORA: 04:03
+ * FINALIDADE DE PROGRAMA: TRABALHAR COM OS OPERADORES ARITMETICOS
+ */
+
+ const [a, b, c, d] = [3, 5, 1, 15];
+
+ const soma = a + b + c + d;
+
+ const subtracao = d -b;
+
+ const multiplicacao = a * b;
+
+ const divisao = d / a;
+
+ const modulo = a % 2;
+
+ console.log(soma, subtracao, divisao, modulo);
+
