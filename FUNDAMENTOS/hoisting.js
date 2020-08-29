40af8929b86127d51fc9ab531e5f056dc8b580af
GAMELEIRA <gameleira270499@gmail.com>
2020-08-29 02:27:33 -0300

    Adicionado o modulo hoisting.js na pasta FUNDAMENTOS que têm como objetivo apresentar os conceitos relacionados ao hoisting

---
+++ FUNDAMENTOS/hoisting.js
@@ -0,0 +1,18 @@
+/**
+ * AUTOR: GABRIEL GAMELEIRA DOS SANTOS
+ * DATA: 29/08/2020
+ * HORA: 02:14
+ * FINALIDADE DE PROGRAMA: APRESENTAR CONCEITOS RELACIONADOS A HOINSTING
+ */
+
+ console.log("a = ", a); //FENOMENO HOINSTING
+
+ var a = 2;
+
+ console.log("a = ", a);
+
+ console.log("b = ", b);
+
+ let b = 2;
+
+ console.log("b = ", b);
