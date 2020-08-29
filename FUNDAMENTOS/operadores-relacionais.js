90f993108ae970d78cda08287ebefee2eeb204b1
GAMELEIRA <gameleira270499@gmail.com>
2020-08-29 05:23:38 -0300

    Adicionado o modulo operadores-relacionais.js na pasta FUNDAMENTOS que têm como objetivo trabalhar com operadores relacionais

---
+++ FUNDAMENTOS/operadores-relacionais.js
@@ -0,0 +1,39 @@
+/**
+ * AUTOR: GABRIEL GAMELEIRA DOS SANTOS
+ * DATA: 25/08/2020
+ * HORA: 05:22
+ * FINALIDADE DE PROGRAMA: TRABALHAR COM OS OPERADORES REALCIONAIS
+ */
+
+ console.log("01)", "1" == 1);
+
+ console.log("02)", "1" === 1); //EXTRITAMENTE IGUAL
+
+ console.log("03)", "3" != 3);
+
+ console.log("04)", "3" !== 3);
+
+ console.log("05)", 3 < 2);
+
+ console.log("06)", 3 > 2);
+
+ console.log("07)", 3 <= 2);
+
+ console.log("08)", 3 >= 2);
+
+ const d1 = new Date(0);
+
+ const d2 = new Date(0);
+
+ console.log("09)", d1 === d2); //ENDERECO DE MEMORIA
+
+ console.log("10)", d1 == d2); //ENDERECO DE MEMORIA
+
+ console.log("11)", d1.getTime() == d2.getTime()); 
+
+ console.log("12)", undefined == null); 
+
+ console.log("12)", undefined === null); 
+ 
+
+
