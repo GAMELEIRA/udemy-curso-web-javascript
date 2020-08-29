24a3b71d6b71e55610beed6850a4fcd4e679a213
GAMELEIRA <gameleira270499@gmail.com>
2020-08-29 01:05:25 -0300

    Adicionado o modulo funcao-quase-tudo.js na pasta FUNDAMENTOS que têm como objetivo apresentar os conceitos relacionados a funções

---
+++ FUNDAMENTOS/funcao-quase-tudo.js
@@ -0,0 +1,12 @@
+/**
+ * AUTOR: GABRIEL GAMELEIRA DOS SANTOS
+ * DATA: 29/08/2020
+ * HORA: 00: 57
+ * FINALIDADE DE PROGRAMA: APRESENTAR CONCEITOS RELACIONADOS A FUNCOES
+ */ 
+
+console.log(typeof Object);
+
+class Produto {}
+
+console.log(typeof Produto);
