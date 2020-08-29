424a2bc8f258563e4f77fd18e448305008aa7901
GAMELEIRA <gameleira270499@gmail.com>
2020-08-29 01:47:21 -0300

    Adicionado o modulo usandoVar2.js na pasta FUNDAMENTOS que têm como objetivo apresentar os conceitos relacionados a utilização de váriaveis

---
+++ FUNDAMENTOS/usandoVar2.js
@@ -0,0 +1,18 @@
+/**
+ * AUTOR: GABRIEL GAMELEIRA DOS SANTOS
+ * DATA: 29/08/2020
+ * HORA: 01:42
+ * FINALIDADE DE PROGRAMA: APRESENTAR CONCEITOS RELACIONADOS A UTILIZACAO DE VARIAVEIS
+ */
+
+ var numero = 1; 
+
+ {
+
+    var numero = 2;
+
+    console.log("Dentro = ", numero);//IRA IMPRIMIR O NUMERO DOIS 
+
+ }
+
+ console.log("Fora = ", numero);//IRA PEGAR O NUMERO DOIS
