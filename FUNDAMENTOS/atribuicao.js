58d2fdbdbfbca5e1114a1fe12d5ccc8878d86f9f
GAMELEIRA <gameleira270499@gmail.com>
2020-08-29 04:03:07 -0300

    Adicionado o modulo atribuicao.js na pasta FUNDAMENTOS que têm como objetivo apresentar os conceitos em relacao a atribuicao de valores

---
+++ FUNDAMENTOS/atribuicao.js
@@ -0,0 +1,36 @@
+/**
+ * AUTOR: GABRIEL GAMELEIRA DOS SANTOS
+ * DATA: 25/08/2020
+ * HORA: 03:
+ * FINALIDADE DE PROGRAMA: APRESENTAR A SINTAXE DE ATRIBUICAO
+ */ 
+
+ const a = 7;
+
+ let b = 3;
+
+ b += a; //IGUAL A b = b + a
+
+ console.log(b);
+
+ b -= 4; //IGUAL A b = b - a
+
+ console.log(b);
+
+ b *= a; //IGUAL A b = b * a
+
+ console.log(b);
+
+ b /= a; //IGUAL A b = b / a
+ 
+ console.log(b);
+
+ b %= 2; //IGUAL A b = b % a
+  
+ console.log(b);
+
+
+
+
+
+ 
