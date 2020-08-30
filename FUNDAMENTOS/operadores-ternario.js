ee5d4e41635fd52fa0118b5baf282a8aac1581ab
GAMELEIRA <gameleira270499@gmail.com>
2020-08-30 01:54:19 -0300

    Adicionando a pasta FUNDAMENTOS  o modulo operadores-ternario.js que têm como objetivo apresentar o comportamento dos operadores ternarios e sua utilização alternativa a estrutura de seleção encadeada if else.

---
+++ FUNDAMENTOS/operadores-ternario.js
@@ -0,0 +1,14 @@
+/**
+ * AUTOR: GABRIEL GAMELEIRA DOS SANTOS
+ * DATA: 30/08/2020
+ * HORA: 01:43
+ * FINALIDADE DE PROGRAMA: TRABALHAR COM OS OPERADORES TERNARIO
+*/
+
+                //EXPRESSAO           //CASO TRUE   //CASO FALSE
+const resultado = nota => nota >= 7 ? "Aprovado" : "Reprovado";
+
+console.log(resultado(7.1));
+
+console.log(resultado(6.7));
+
