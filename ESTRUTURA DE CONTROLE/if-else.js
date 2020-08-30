3f209d9bf5ec79298d668c600d7e7ddadde17625
GAMELEIRA <gameleira270499@gmail.com>
2020-08-30 03:22:56 -0300

    Criada pasta ESTRUTURA DE CONTROLE  que contera os modulos que terão como foca trabalhar com as estruturas de controle em JS. Adicionado na pasta ESTRUTURA DE CONTROLE  o modulo if2.js que têm como objetivo apresentar a estrutura simples de um código if e else

---
+++ ESTRUTURA DE CONTROLE/if-else.js
@@ -0,0 +1,19 @@
+/**
+ * AUTOR: GABRIEL GAMELEIRA DOS SANTOS
+ * DATA: 30/08/2020
+ * HORA: 03:22
+ * FINALIDADE DE PROGRAMA: TRABALHAR COM A ESTRUTURA DE CONTROLE IF E ELSE
+ */
+
+const imprimirResultado = function(nota) {
+
+    if(nota => 7) {
+
+        console.log("Aprovado!");
+
+    } else {
+
+        console.log("Reprovado!");
+
+    }
+}
