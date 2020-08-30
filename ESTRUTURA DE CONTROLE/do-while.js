b82c06d012162eed3aea2b2424f37c00a65158fb
GAMELEIRA <gameleira270499@gmail.com>
2020-08-30 04:15:07 -0300

    Criada pasta ESTRUTURA DE CONTROLE  que contera os modulos que terão como foca trabalhar com as estruturas de controle em JS. Adicionado na pasta ESTRUTURA DE CONTROLE  o modulo do- while.js que têm como objetivo apresentar a estrutura repeticao  simples de um código do while

---
+++ ESTRUTURA DE CONTROLE/do-while.js
@@ -0,0 +1,26 @@
+/**
+ * AUTOR: GABRIEL GAMELEIRA DOS SANTOS
+ * DATA: 30/08/2020
+ * HORA: 03:22
+ * FINALIDADE DE PROGRAMA: TRABALHAR COM A ESTRUTURA DE REPETICAO DO WHILE
+ */
+
+function getInteiroAleatorioEntre(min, max) {
+
+    const valor = Math.random() * (max - min) + min;
+
+    return Math.floor(valor);
+
+}
+
+let opcao = 0;
+
+do {
+
+    opcao = getInteiroAleatorioEntre(-1, 10);
+
+    console.log(`Opção escolhida foi ${opcao}`);
+
+} while (opcao != -1);
+
+console.log("Até a próxima!");
