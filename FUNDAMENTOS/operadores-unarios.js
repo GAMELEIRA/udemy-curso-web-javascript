7110bd3dbef38a789503ce7aa23a7d11d2a9512f
GAMELEIRA <gameleira270499@gmail.com>
2020-08-30 01:41:13 -0300

    Adicionando a pasta FUNDAMENTOS  o modulo operadores-unarios.js que têm como objetivo apresentar o comportamento dos operadores unarios.

---
+++ FUNDAMENTOS/operadores-unarios.js
@@ -0,0 +1,22 @@
+/**
+ * AUTOR: GABRIEL GAMELEIRA DOS SANTOS
+ * DATA: 30/08/2020
+ * HORA: 01:35
+ * FINALIDADE DE PROGRAMA: TRABALHAR COM OS OPERADORES UNARIOS
+*/
+
+let num1 = 1;
+
+let num2 = 2;
+
+num1++; //POS FIXADA
+
+console.log(num1);
+
+--num1; //PRE FIXADA
+
+console.log(num1);
+
+console.log(++num1 === num2--);
+
+console.log(num1 === num2);
