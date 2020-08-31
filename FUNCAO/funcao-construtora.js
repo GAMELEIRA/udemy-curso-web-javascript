e4228403c90a2415f92bd4f0cc1bce067aec6fda
GAMELEIRA <gameleira270499@gmail.com>
2020-08-30 22:14:14 -0300

    Criado na pasta FUNCAO  o modulo  funcoes-construtoras.js que têm como objetivo apresentar estrategias de como trabalhar com callbacks.

---
+++ FUNCAO/funcao-construtora.js
@@ -0,0 +1,56 @@
+/**
+ * AUTOR: GABRIEL GAMELEIRA DOS SANTOS
+ * DATA: 30/08/2020
+ * HORA: 21:37
+ * FINALIDADE DE PROGRAMA: TRABALHAR COM FUNCOES CONSTRUTORAS
+*/
+
+function Carro(velocidadeMaxima = 200, delta = 5) {
+
+    //ATRIBUTO INTERNO
+    let velocidadeAtual = 0;
+
+    //METODO PUBLICO
+    this.acelerar = function () {
+
+        if (velocidadeAtual + delta <= velocidadeMaxima) {
+
+            velocidadeAtual += delta;
+
+        } else {
+
+            velocidadeAtual = velocidadeMaxima;
+
+        }
+
+    }
+
+    this.getVelocidadeAtual = function () {
+
+        return velocidadeAtual;
+
+    }
+
+   this.validarInstancia = function (instancia) {
+
+        return this === instancia;
+
+    }
+
+}
+
+const uno = new Carro();
+
+uno.acelerar();
+
+console.log(uno.getVelocidadeAtual());
+
+const ferrari = new Carro(350, 20);
+
+ferrari.acelerar();
+
+ferrari.acelerar();
+
+ferrari.acelerar();
+
+console.log(ferrari.getVelocidadeAtual());
