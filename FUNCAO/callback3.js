8594281369e11dd7024574765d42b8ef150f947a
GAMELEIRA <gameleira270499@gmail.com>
2020-08-30 21:45:30 -0300

    Criado na pasta FUNCAO  o modulo  callback3.js que têm como objetivo apresentar estrategias de como trabalhar com callbacks.

---
+++ FUNCAO/callback3.js
@@ -0,0 +1,8 @@
+/**
+ * AUTOR: GABRIEL GAMELEIRA DOS SANTOS
+ * DATA: 30/08/2020
+ * HORA: 21:37
+ * FINALIDADE DE PROGRAMA: TRABALHAR COM CALLBACKS
+*/
+
+document.getElementsByTagName('body')[0].onclick = (e) => console.log("Clicou!!!");
