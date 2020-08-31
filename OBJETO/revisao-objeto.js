873e831d561342ee16fcd72028e02d813b83c3af
GAMELEIRA <gameleira270499@gmail.com>
2020-08-31 02:22:46 -0300

    Criado a pasta OBEJTO que terá como objeto trabalhar com a notação de criação de objetos. Adionado na pasta OBJETCO  o modulo revisao-objeto.js que têm como objetivo entender a notação de objeto

---
+++ OBJETO/revisao-objeto.js
@@ -0,0 +1,80 @@
+/**
+ * AUTOR: GABRIEL GAMELEIRA DOS SANTOS
+ * DATA: 31/08/2020
+ * HORA: 02:02
+ * FINALIDADE DE PROGRAMA: TRABALHAR COM OS OPERADORES REALACIONAIS
+ */
+
+ const produto = new Object;
+
+ produto.nome = "Cadeira";
+
+ produto["Marca do produto"] = "Genérica"
+
+ produto.preco = 220;
+
+ console.log(produto);
+
+ delete produto.preco;
+
+ delete produto[`marca do produto`];
+
+ console.log(produto);
+
+ const carro = {
+
+    modelo: "A4",
+
+    valor: 89000,
+
+    proprietario: {
+
+        nome: "Raul",
+
+        idade: 56,
+
+        endereco: {
+
+            logradouro: "Rua ABC",
+
+            numero: 123
+
+        }
+        
+    },
+
+    condutores: [{
+        
+        nome: "Junior",
+
+        idade:  19
+
+    }, {
+
+        nome: "Ana",
+
+        idade: 42
+
+
+    }],
+
+    calcularValorSeguro: function(){}
+
+ }
+
+ carro.proprietario.endereco.numero = 1000;
+
+ carro["proprietario"]["endereco"]["numero"];
+
+ console.log(carro);
+
+ delete carro.condutores;
+
+ delete carro.proprietario.endereco;
+
+ delete carro.calcularValorSeguro;
+
+ console.log(carro);
+
+ console.log(carro.condutores);
+
