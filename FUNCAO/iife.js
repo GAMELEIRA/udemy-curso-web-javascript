180a877c7b3742182ddddc1bc292ca586ff120fb
GAMELEIRA <gameleira270499@gmail.com>
2020-08-30 23:56:17 -0300

    Criado na pasta FUNCAO  o modulo iife.js que têm como objetivo entender o que são funções auto invocadas

---
+++ FUNCAO/iife.js
@@ -0,0 +1,16 @@
+/**
+ * AUTOR: GABRIEL GAMELEIRA DOS SANTOS
+ * DATA: 30/08/2020
+ * HORA: 23: 44
+ * FINALIDADE DE PROGRAMA: TRABALHAR COM FUNCAO AUTO INVOCADA
+*/
+
+//IIFE -> IMMEDIATELY INVOKED FUNCTION EXPRESSION
+
+(function() {
+
+    console.log("Será executado na hora!");
+
+    console.log("Foge do escopo mais abrangente");
+
+})();
