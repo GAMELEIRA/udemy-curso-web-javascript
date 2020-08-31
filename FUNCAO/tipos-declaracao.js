5088cc871618e19e922a443c79a0149242226203
GAMELEIRA <gameleira270499@gmail.com>
2020-08-30 22:23:33 -0300

    Criado na pasta FUNCAO  o modulo  tipos-declaracao.js que têm como objetivo apresentar estrategias de como trabalhar com declaracoes de funcoes.

---
+++ FUNCAO/tipos-declaracao.js
@@ -0,0 +1,27 @@
+/**
+ * AUTOR: GABRIEL GAMELEIRA DOS SANTOS
+ * DATA: 30/08/2020
+ * HORA: 20:15
+ * FINALIDADE DE PROGRAMA: TRABALHAR COM OS TIPOS DE DECLARACAO
+*/
+
+//FUNCTION DECLATION
+function soma(x, y) {
+
+    return x + y;
+
+}
+
+//FUNCTION EXPRESSION
+const sub = function (x, y) {
+
+    return x - y;
+
+}
+
+//NAMED FUNCTION EXPRESSION
+const mult = function mult(x, y) {
+
+    return x * y;
+
+}
