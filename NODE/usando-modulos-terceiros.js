57979b82b324dd85943c21cffff9a1445721eae7
GAMELEIRA <gameleira270499@gmail.com>
2020-09-20 04:13:05 -0300

    Adicionado na pasta NODE o modulo usando-modulos-terceiros.js que têm como objetivo trabalhar com a importação de modulos de terceiros usando node

---
+++ NODE/usando-modulos-terceiros.js
@@ -0,0 +1,10 @@
+/**
+ * AUTOR: GABRIEL GAMELEIRA DOS SANTOS
+ * DATA: 20/09/2020
+ * HORA: 04:03
+ * FINALIDADE DE PROGRAMA: TRABALHAR COM IMPORTACAO DE INSTRUCOES ENTRE MODULOS DE TERCEIROS
+ */
+
+ const _ = require('lodash');
+
+ setInterval(() => console.log(_.random(1, 1000)), 2000);
