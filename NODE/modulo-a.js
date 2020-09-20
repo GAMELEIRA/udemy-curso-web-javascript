c0e267254868b446937328bde798e17131d8a957
GAMELEIRA <gameleira270499@gmail.com>
2020-09-20 03:46:03 -0300

    Adicionado em NODE o  modulo-a.js que têm como objetivo trabalhar com a exportação de instruções em node

---
+++ NODE/modulo-a.js
@@ -0,0 +1,12 @@
+/**
+ * AUTOR: GABRIEL GAMELEIRA DOS SANTOS
+ * DATA: 20/09/2020
+ * HORA: 03:35
+ * FINALIDADE DE PROGRAMA: TRABALHAR COM EXPORTACAO DE INSTRUCOES ENTRE MODULOS
+ */
+
+ this.ola = "Fala Pessoal";
+
+ exports.bemVindo = "Bem vindo ao node";
+
+ module.exports.ateLogo = "Até o próximo exemplo";
