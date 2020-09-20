642b46df688abbe9be8d061050a3ba178cf1208b
GAMELEIRA <gameleira270499@gmail.com>
2020-09-20 05:52:35 -0300

    Adicionado na pasta NODE o modulo funcionarios.js que têm como objetivo encontrar a mulher chinesa com o menor salario

---
+++ NODE/funcionarios/funcionarios.js
@@ -0,0 +1,31 @@
+/**
+ * AUTOR: GABRIEL GAMELEIRA DOS SANTOS
+ * DATA: 20/09/2020
+ * HORA: 05:49
+ * FINALIDADE DE PROGRAMA: BUSCAR A MULHER CHINESA COM O MENOR SALARIO
+ */
+
+const url = 'http://files.cod3r.com.br/curso-js/funcionarios.json';
+
+const axios = require("axios");
+
+const buscarMulheres = (funcionarios) => funcionarios.filter(funcionario => funcionario.genero == "M");
+
+const buscarMulheresChinesas = (mulheres) => mulheres.filter(mulher => mulher.pais == "China");
+
+const buscarMenorSalarioMulheresChinesas = (chinesas) => chinesas.reduce((chinesaAnterior, chinesaAtual) => (chinesaAnterior.salario <= chinesaAtual.salario)? chinesaAnterior: chinesaAtual);
+
+axios.get(url).then(response => {
+
+    const funcionarios = response.data;
+
+   const mulheres = buscarMulheres(funcionarios)
+   
+   const mulheresChinesas = buscarMulheresChinesas(mulheres);
+
+   const mulherChinesaSalarioMenor = buscarMenorSalarioMulheresChinesas(mulheresChinesas);
+
+   console.log(mulherChinesaSalarioMenor);
+
+})
+
