4be25ebf60ec0ca263a29f7267217cce36dec59f
GAMELEIRA <gameleira270499@gmail.com>
2020-09-20 03:47:25 -0300

    Adicionado em NODE o  modulo-b.js que têm como objetivo trabalhar com a exportação de instruções em node

---
+++ NODE/modulo-b.js
@@ -0,0 +1,18 @@
+/**
+ * AUTOR: GABRIEL GAMELEIRA DOS SANTOS
+ * DATA: 20/09/2020
+ * HORA: 03:46
+ * FINALIDADE DE PROGRAMA: TRABALHAR COM EXPORTACAO DE INSTRUCOES ENTRE MODULOS
+ */
+
+module.exports = {
+
+    bomDia: "Bom dia",
+
+    boaNoite () {
+
+        return "Boa noite";
+
+    }
+
+ }
