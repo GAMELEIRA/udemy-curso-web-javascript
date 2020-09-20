a9f1b06a2df58d0d7d4bf840d6de4e0744bdc6d8
GAMELEIRA <gameleira270499@gmail.com>
2020-09-20 07:16:29 -0300

    Entendendo um pouco mais sobre scripts do packge.json

---
+++ NODE/passando-parametros-cliente.js
@@ -0,0 +1,5 @@
+const { stubArray } = require("lodash");
+
+const saudacoes = require("./passando-parametros")("ANA", "LUCAS", 'JOÃO');
+
+console.log(saudacoes); 
---
+++ NODE/passando-parametros.js
@@ -0,0 +1,12 @@
+/**
+ * AUTOR: GABRIEL GAMELEIRA DOS SANTOS
+ * DATA: 20/09/2020
+ * HORA: 06:24
+ * FINALIDADE DE PROGRAMA: 
+ */
+
+ module.exports = function(...nomes) {
+
+    return nomes.map(nome => `Boa semana ${nome}`);
+
+ }
